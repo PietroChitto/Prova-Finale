af8e209dc20718b7e658e49f51610bd866f0494b
Pietro <pietrochitto@hotmail.it>
2017-06-12 11:46:49 +0200

    new  version
    Signed-off-by: Pietro <pietrochitto@hotmail.it>

---
+++ src/partita/eccezioniPartita/TorreOccupataException.java
@@ -0,0 +1,11 @@
+package partita.eccezioniPartita;
+
+/**
+ * Created by Pietro on 01/06/2017.
+ */
+public class TorreOccupataException extends Exception{
+    public TorreOccupataException(){
+        super();
+        //vnjsvdkuhf lnvk l
+    }
+}
